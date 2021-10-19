15d071cedab68a2e1387be073970c3caa3f32c39
ariel <arielgenua24@gmail.com>
2021-10-19 15:04:03 -0300

    pase el algoritmo de recomendacion de exampleFileAlgorithm a recomendationsAlgorithm.ts

--- src/recomendatiosAlgorithm.ts
+++ src/recomendatiosAlgorithm.ts
@@ -1,16 +1,122 @@
 import {recomendations, steveJobs_George, steveJobs_WalterIsaacson,RobertKiyosaki, RobertKiyosaki2, empresas, psicologia, tecnologia, Marketing } from './userRecomendations';
 
-function recomendationsAlgorithm(list:string[]){
-    //alguien toca una determinada cantidad de addEventListener.
-    //cada boton tocada de addevent listener tiene una data diferente
-    // esa data llega a esta funcion en un array(que es el parametro que se pide), esta misma data apunta a una determinada cantidad de libros para recomendar
-    //traigo esos libros que a el usuario le gustaria saber que le recomienden
-    //seguramente son muchos libros, por lo tanto, lo mejor es recomendar:
-    //si toca en 2 botones, recomendamos 6 libros, 3 de cada boton
-    //si toca 3 boton, 6 libros, 2 de cada boton
-    // y si toca en 4 botones, recomednamos 8 libros, 2 de cada boton.
 
+const arrayRecomendations: string[] = [];
+
+
+
+function userChoicesRecomendations(){
+
+    const arrayPeopleSelected: string[] = [];
+
+    const section_div_people__Node:any = document.querySelector('.section_div__people')
+    const fatherNode:any = document.querySelector('.section_div_choices')
+    const endChoicesButton:any = document.querySelector('#button_endChoices')
+
+    const jobsRecomendations:any = document.querySelector('#button_jobs');
+    const tecnologiaRecomendations:any = document.querySelector('#button_tecnologia');
+    const RobertKiyosakiRecomendations:any = document.querySelector('#button_RobertKiyosaki');
+    const marketingRecomendations:any = document.querySelector('#button_Marketing');
+    const psicologiaRecomendations:any = document.querySelector('#button_Psicologia');
+    const empresasRecomendations:any = document.querySelector('#button_empresas');
+    
+
+
+    function ShowUserChoicesResume(){
+        let div:any = document.createElement('div');
+        div = '';
+
+        arrayPeopleSelected.forEach(element => {
+            div = div + ` ${element} `
+        });
+        section_div_people__Node.append(div);
+    }
+
+
+
+
+    endChoicesButton.addEventListener('click', ()=> {
+        section_div_people__Node.removeChild(fatherNode);
+
+        ShowUserChoicesResume();
+
+    })
+
+
+
+
+
+    let accion = () => {
+        console.log(`tengo una serpiente en mi bota`)
+        console.log(arrayRecomendations)
+        console.log(arrayPeopleSelected)
+
+    }
 
 
     
+    jobsRecomendations.addEventListener('click', (event:undefined) => {
+        const span_jobs:any = document.querySelector('.span_button-Jobs');
+        arrayPeopleSelected.push(span_jobs.innerHTML);
+
+        accion();
+        
+        fatherNode.removeChild(jobsRecomendations)
+        
+    })
+
+    tecnologiaRecomendations.addEventListener('click',(event:undefined) => {
+        const span_tecnologia:any = document.querySelector('.span_button-Tecnologia');
+        arrayPeopleSelected.push(span_tecnologia.innerHTML);
+        
+        accion();
+        fatherNode.removeChild(tecnologiaRecomendations)
+        
+
+    })
+    RobertKiyosakiRecomendations.addEventListener('click',(event:undefined) => {
+        const span_RobertKiyosaki:any = document.querySelector('.span_button-RobertKiyosaki');
+        arrayPeopleSelected.push(span_RobertKiyosaki.innerHTML);
+
+
+        accion();
+        fatherNode.removeChild(RobertKiyosakiRecomendations)
+        
+
+    })
+    marketingRecomendations.addEventListener('click', (event:undefined) => {
+        const span_Marketing:any = document.querySelector('.span_button-Marketing');
+        arrayPeopleSelected.push(span_Marketing.innerHTML);
+
+
+        accion();
+        fatherNode.removeChild(marketingRecomendations)
+        
+
+    })
+    psicologiaRecomendations.addEventListener('click', (event:undefined) => {
+
+        const span_Psicologia:any = document.querySelector('.span_button-Psicologia');
+        arrayPeopleSelected.push(span_Psicologia.innerHTML);
+
+        accion();
+        fatherNode.removeChild(psicologiaRecomendations)
+        
+
+    })
+    empresasRecomendations.addEventListener('click',(event:undefined) => {
+
+        const span_Empresas:any = document.querySelector('.span_button-Empresas');
+        arrayPeopleSelected.push(span_Empresas.innerHTML);
+
+        accion();
+        fatherNode.removeChild(empresasRecomendations)
+        
+        
+    })
+
+
+
 }
+userChoicesRecomendations();
+
