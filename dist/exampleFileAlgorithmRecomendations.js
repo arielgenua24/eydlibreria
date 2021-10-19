92a0e5f470fb15a31b5ae971b3f941a9ba4d92f4
ariel <arielgenua24@gmail.com>
2021-10-19 00:49:39 -0300

    cree un boton de listo y sus funciones correspondientes que le permitan ver al usuario que clase de elecciones tomo, para asi recomendarle libros de manera efectiva

--- dist/exampleFileAlgorithmRecomendations.js
+++ dist/exampleFileAlgorithmRecomendations.js
@@ -1,23 +1,53 @@
-console.log(`hola`)
+
 
 jobs = {
     libros: ['la lalalal prueba 1', 'jobs j']
 }
 
 const arrayRecomendations = [];
-const arrayPeopleSelected = [];
+
 
 
 function userChoicesRecomendations(){
 
-    
-    const fatherNode = document.querySelector('.section_div__people')
+    const arrayPeopleSelected = [];
+
+    const section_div_people__Node = document.querySelector('.section_div__people')
+    const fatherNode = document.querySelector('.section_div_choices')
     const jobsRecomendations = document.querySelector('#button_jobs');
     const tecnologiaRecomendations = document.querySelector('#button_tecnologia');
     const RobertKiyosakiRecomendations =document.querySelector('#button_RobertKiyosaki');
     const marketingRecomendations =document.querySelector('#button_Marketing');
     const psicologiaRecomendations =document.querySelector('#button_Psicologia');
     const empresasRecomendations =document.querySelector('#button_empresas');
+    const endChoicesButton = document.querySelector('#button_endChoices')
+
+
+    
+
+
+    function userChoicesResume(){
+        let div = document.createElement('div');
+        div = '';
+
+        arrayPeopleSelected.forEach(element => {
+            div = div + ` ${element} `
+        });
+        section_div_people__Node.append(div);
+    }
+
+
+
+
+    endChoicesButton.addEventListener('click', ()=> {
+        section_div_people__Node.removeChild(fatherNode);
+
+        userChoicesResume();
+
+    })
+
+
+
 
 
     let accion = () => {
@@ -25,6 +55,8 @@ function userChoicesRecomendations(){
         const data = jobs.libros;
         arrayRecomendations.push(data)
         console.log(arrayRecomendations)
+        console.log(arrayPeopleSelected)
+
     }
 
 
@@ -40,8 +72,8 @@ function userChoicesRecomendations(){
     })
 
     tecnologiaRecomendations.addEventListener('click',(event) => {
-        const span_jobs = document.querySelector('.span_button-Jobs');
-        arrayPeopleSelected.push(span_jobs.innerHTML);
+        const span_tecnologia = document.querySelector('.span_button-Tecnologia');
+        arrayPeopleSelected.push(span_tecnologia.innerHTML);
         
         accion();
         fatherNode.removeChild(tecnologiaRecomendations)
@@ -49,33 +81,47 @@ function userChoicesRecomendations(){
 
     })
     RobertKiyosakiRecomendations.addEventListener('click',(event) => {
+        const span_RobertKiyosaki = document.querySelector('.span_button-RobertKiyosaki');
+        arrayPeopleSelected.push(span_RobertKiyosaki.innerHTML);
+
+
         accion();
         fatherNode.removeChild(RobertKiyosakiRecomendations)
         
 
     })
     marketingRecomendations.addEventListener('click', (event) => {
+        const span_Marketing = document.querySelector('.span_button-Marketing');
+        arrayPeopleSelected.push(span_Marketing.innerHTML);
+
+
         accion();
         fatherNode.removeChild(marketingRecomendations)
         
 
     })
     psicologiaRecomendations.addEventListener('click', (event) => {
+
+        const span_Psicologia = document.querySelector('.span_button-Psicologia');
+        arrayPeopleSelected.push(span_Psicologia.innerHTML);
+
         accion();
         fatherNode.removeChild(psicologiaRecomendations)
         
 
     })
     empresasRecomendations.addEventListener('click',(event) => {
+
+        const span_Empresas = document.querySelector('.span_button-Empresas');
+        arrayPeopleSelected.push(span_Empresas.innerHTML);
+
         accion();
         fatherNode.removeChild(empresasRecomendations)
         
         
     })
-    
-    
 
-    console.log(arrayPeopleSelected)
+
 
 }
 userChoicesRecomendations();
--- src/books.ts
+++ src/books.ts
@@ -5,6 +5,7 @@ class books extends booksCategories{
     bookName: string;
     author: string;
     description: string;
+    image: string;
 
     constructor(
         categoryName: string,
@@ -12,17 +13,23 @@ class books extends booksCategories{
         bookName: string,
         author: string,
         description: string,
+        image: string
         ){
             super(categoryName, categoryTheme);
             this.bookName = bookName;
             this.author = author;
             this.description = description;
+            this.image = image;
         }
 }
 
-const habitos_para_ser_millonario = new books(desarrolloPersonal.categoryName,desarrolloPersonal.categoryTheme,'Habitos para ser millonario','Brian Tracy','¿Por qué algunas personas son más exitosas que otras? El 95 % de todo lo que piensas, sientes, haces y logras es resultado del hábito. Simplificando y organizando las ideas, Brian Tracy, experto en temas de liderazgo, estrategia, creatividad y psicología del éxito, ha escrito magistralmente un libro de obligada lectura sobre hábitos que asegura completamente el éxito personal.')
+//Nota, la imagen a demas de ser relevante en los objetos de la libreria, lo es a la hora de poder aplicarla de manera efectiva en una estructura de datos  que me permita mostrar este misma en el DOM de manera efectiva.
 
-const como_hacer_que_la_gente_haga_cosas = new books(liderazgo.categoryName,liderazgo.categoryTheme,'Como hacer que la gente haga cosas','Robert Conkin','La clave para convencer,dirigir,motivar,supervisar e influir en los demas')
+//nota 2, la imagen no es una string, es un conjunto de datos, que addquieren tanto una string como vinculos determinados.
+
+const habitos_para_ser_millonario = new books(desarrolloPersonal.categoryName,desarrolloPersonal.categoryTheme,'Habitos para ser millonario','Brian Tracy','¿Por qué algunas personas son más exitosas que otras? El 95 % de todo lo que piensas, sientes, haces y logras es resultado del hábito. Simplificando y organizando las ideas, Brian Tracy, experto en temas de liderazgo, estrategia, creatividad y psicología del éxito, ha escrito magistralmente un libro de obligada lectura sobre hábitos que asegura completamente el éxito personal.','src:')
+
+const como_hacer_que_la_gente_haga_cosas = new books(liderazgo.categoryName,liderazgo.categoryTheme,'Como hacer que la gente haga cosas','Robert Conkin','La clave para convencer,dirigir,motivar,supervisar e influir en los demas','src::')
 
 
 
