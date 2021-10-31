856874536534c5d378ac51de393aefbe6b5f5b76
ariel <arielgenua24@gmail.com>
2021-10-31 18:08:23 -0300

    agregue el codigo necesario para que la funcion del archivo showSearched que muestra los libros que el usario busco, trabaje correctamente.

--- dist/showSearchedBooks.js
+++ dist/showSearchedBooks.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const searchBooks_1 = require("./searchBooks");
 const father_node = document.querySelector('.main_section');
 const section_explore = document.querySelector('.section_explore');
-function delete_home() {
+function delete_section_explore() {
     try {
         father_node.removeChild(section_explore);
     }
@@ -14,33 +14,51 @@ function delete_home() {
 const search_section__Container = [];
 const search_section = document.createElement('section');
 search_section.className = 'search_section';
-function openSearchWindow() {
-    console.log(search_section__Container.length);
+let counter = 0;
+function openSearchWindow(param) {
+    const div__text__container = document.createElement('div');
+    const h2 = document.createElement('h2');
+    h2.textContent = `Tu busqueda numero ${counter} es:`;
+    div__text__container.appendChild(h2);
+    const div__book__Container = document.createElement('div');
     if (search_section__Container.length < 1) {
         search_section__Container.push(search_section);
-        console.log(search_section__Container.length);
         father_node.appendChild(search_section);
-        console.log(search_section__Container);
+        search_section.appendChild(div__book__Container);
     }
-    else {
-        console.log(`NO ESTOY ELIMINANDOOOO`);
-        father_node.removeChild(search_section);
-        father_node.appendChild(search_section);
-    }
+    console.log(`ESTOY ELIMINANDOOOO`);
+    search_section.removeChild(div__book__Container);
+    father_node.removeChild(search_section);
+    param.forEach((el) => {
+        console.log(el);
+        div__book__Container.className = 'div__book__Container';
+        const paragraph = document.createElement('p');
+        paragraph.className = 'div__span-fontSize';
+        paragraph.textContent = `${el.bookName}`;
+        const image = document.createElement('img');
+        image.src = `${el.image}`;
+        image.className = "image_size";
+        div__book__Container.append(paragraph, image);
+        console.log(el);
+    });
+    father_node.appendChild(search_section);
+    search_section.append(div__book__Container, div__text__container);
 }
 const searchBar = document.getElementById('searchBar');
 searchBar.addEventListener('keyup', (event) => {
-    let text = event.target.value;
-    if (text.length == 0) {
+    let userText = event.target.value;
+    if (userText.length == 0) {
         father_node.removeChild(search_section);
         search_section__Container.pop();
     }
-    if (text.length > 0) {
-        delete_home();
+    if (userText.length > 0 && userText.length < 2) {
+        delete_section_explore();
     }
-    const response = (0, searchBooks_1.searchBooks)(text);
+    const response = (0, searchBooks_1.searchBooks)(userText);
+    console.log(response);
     if (response) {
         console.log(`siiii`);
-        openSearchWindow();
+        openSearchWindow(response);
+        counter = counter + 1;
     }
 });
--- src/showSearchedBooks.ts
+++ src/showSearchedBooks.ts
@@ -5,7 +5,7 @@ const father_node:any = document.querySelector('.main_section')
 const section_explore:any = document.querySelector('.section_explore');
 
 
-    function delete_home() {
+    function delete_section_explore() {
         try {
             father_node.removeChild(section_explore);
         }
@@ -13,37 +13,70 @@ const section_explore:any = document.querySelector('.section_explore');
             console.log(err)
         }
        
-    }
+    } 
 
 
 const search_section__Container:any[] = [];
 const search_section = document.createElement('section')
 search_section.className = 'search_section'
 
-function openSearchWindow() {
-    
+let counter = 0;
 
-    console.log(search_section__Container.length)
-    
+
+function openSearchWindow(param:any[]) {
+        const div__book__Container = document.createElement('div')
+
+
+        const div__text__container = document.createElement('div')
+        const h2 = document.createElement('h2')
+        h2.textContent = `Tu busqueda numero ${counter} es:`
+        h2.className = 'h2__search--size'
+        div__text__container.appendChild(h2)
+
+        
+        
 
     if(search_section__Container.length < 1 ) {
-        
+
         search_section__Container.push(search_section)
-        console.log(search_section__Container.length)
         father_node.appendChild(search_section)
+        search_section.appendChild(div__book__Container)
 
-        console.log(search_section__Container)
-
-    } else {
-        console.log(`NO ESTOY ELIMINANDOOOO`)
+     }
+        console.log(`ESTOY ELIMINANDOOOO`)
+            
+        search_section.removeChild(div__book__Container)
 
         father_node.removeChild(search_section)
+               
+        
+        param.forEach((el:any) => {
+            
+            console.log(el)
+            div__book__Container.className = 'div__book__Container'
+
+            const paragraph = document.createElement('p')
+            paragraph.className = 'div__span-fontSize'
+            paragraph.textContent = `${el.bookName}`
+
+            const image = document.createElement('img')
+            image.src = `${el.image}`
+            image.className = "image_size"
+
+            div__book__Container.append(paragraph,image)
+            console.log(el)
+        });
+
         father_node.appendChild(search_section)
+        search_section.append(div__book__Container,div__text__container)
         
         
-    }
+        
     
 }
+
+
+
 
 const searchBar:any = document.getElementById('searchBar');
 
@@ -51,23 +84,29 @@ const searchBar:any = document.getElementById('searchBar');
     searchBar.addEventListener('keyup',(event:any) => {
     
     
-    let text = event.target.value;
+    let userText = event.target.value;
     
-    if(text.length == 0) {
+    if(userText.length == 0) {
+
         father_node.removeChild(search_section)
         search_section__Container.pop();
+
+
+
     }
 
 
-    if(text.length > 0) {
-        delete_home()
+    if(userText.length > 0 && userText.length < 2) {
+        delete_section_explore()
     } 
 
-    const response = searchBooks(text);
+    const response = searchBooks(userText);
+    console.log(response)
     
     if(response) {
         console.log(`siiii`)
-        openSearchWindow()
+        openSearchWindow(response)
+        counter = counter + 1;
     } 
     
 
